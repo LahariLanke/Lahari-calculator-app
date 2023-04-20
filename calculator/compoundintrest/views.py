15dd5d5ea3eee25756ca7345c3113691f13dbfe4
LahariLanke <lahari.marolix@gmail.com>
2023-04-20 13:17:08 -0800

    lahari_compound_int_app

---
+++ calculator/compoundintrest/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def calculate_interest(request):
+    if request.method == 'POST':
+        principal = float(request.POST['principal'])
+        time = float(request.POST['time'])
+        rate = float(request.POST['rate'])
+        compound_interest = principal * (pow((1 + rate / 100), time))
+        context = {'compound_interest': round(compound_interest, 2)}
+        return render(request, 'intrest.html', context)
+    return render(request, 'home.html')
